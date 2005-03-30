925a21f9bd5534e7da36e630f013b0920d507060
Mikhail Gusarov <dottedmag@dottedmag.net>
2005-03-30 14:28:43 +0000

    x2x-1.30-beta imported.

---
+++ lawyerese.c
@@ -0,0 +1,74 @@
+char *lawyerese = 
+#ifdef WIN_2_X
+" Cygwin version with -fromwin to allow source to be a Windows\n" 
+" machine that is not running a X server.\n"
+" Adapted by Mark Hayter 2003 using win2vnc ClientConnect.cpp code\n"
+"\n"
+"The x2x code is coverd by the following:\n\n"
+#endif
+"Copyright (c) 1997\n\
+Digital Equipment Corporation.  All rights reserved.\n\
+\n\
+By downloading, installing, using, modifying or distributing this\n\
+software, you agree to the following:\n\
+\n\
+1. CONDITIONS. Subject to the following conditions, you may download,\n\
+install, use, modify and distribute this software in source and binary\n\
+forms:\n\
+\n\
+a) Any source code, binary code and associated documentation\n\
+(including the online manual) used, modified or distributed must\n\
+reproduce and retain the above copyright notice, this list of\n\
+conditions and the following disclaimer.\n\
+\n\
+b) No right is granted to use any trade name, trademark or logo of\n\
+Digital Equipment Corporation.  Neither the \"Digital Equipment\n\
+Corporation\" name nor any trademark or logo of Digital Equipment\n\
+Corporation may be used to endorse or promote products derived from\n\
+this software without the prior written permission of Digital\n\
+Equipment Corporation.\n\
+\n\
+2.  DISCLAIMER.  THIS SOFTWARE IS PROVIDED BY DIGITAL \"AS IS\" AND ANY\n\
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n\
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n\
+PURPOSE ARE DISCLAIMED.IN NO EVENT SHALL DIGITAL BE LIABLE FOR ANY\n\
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n\
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE\n\
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n\
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER\n\
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n\
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF\n\
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\
+"
+#ifdef WIN_2_X
+"\n"
+"The win2vnc code was itself adapted from vncviewer for windows\n"
+"\n"
+" win2vnc, adapted from vncviewer by Fredrik Hubinette 2001\n"
+"\n"
+" Original copyright follows:\n"
+"\n"
+"  Copyright (C) 1999 AT&T Laboratories Cambridge. All Rights Reserved.\n"
+"\n"
+"  This file is part of the VNC system.\n"
+"\n"
+"  The VNC system is free software; you can redistribute it and/or modify\n"
+"  it under the terms of the GNU General Public License as published by\n"
+"  the Free Software Foundation; either version 2 of the License, or\n"
+"  (at your option) any later version.\n"
+"\n"
+"  This program is distributed in the hope that it will be useful,\n"
+"  but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"  GNU General Public License for more details.\n"
+"\n"
+"  You should have received a copy of the GNU General Public License\n"
+"  along with this program; if not, write to the Free Software\n"
+"  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,\n"
+"  USA.\n"
+"\n"
+" If the source code for the VNC system is not available from the place \n"
+" whence you received this file, check http://www.uk.research.att.com/vnc or contact\n"
+" the authors on vnc@uk.research.att.com for information on obtaining it.\n"
+#endif
+;
