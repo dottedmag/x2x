8694274d91c304f2879783d0f16aed29c9e75bf6
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2012-12-28 17:15:44 +0100

    Imported Upstream version 1.27.svn.20060214

--- lawyerese.c
+++ lawyerese.c
@@ -1,4 +1,11 @@
 char *lawyerese = 
+#ifdef WIN_2_X
+" Cygwin version with -fromwin to allow source to be a Windows\n" 
+" machine that is not running a X server.\n"
+" Adapted by Mark Hayter 2003 using win2vnc ClientConnect.cpp code\n"
+"\n"
+"The x2x code is coverd by the following:\n\n"
+#endif
 "Copyright (c) 1997\n\
 Digital Equipment Corporation.  All rights reserved.\n\
 \n\
@@ -32,4 +39,36 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER\n\
 IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n\
 OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF\n\
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\
-";
+"
+#ifdef WIN_2_X
+"\n"
+"The win2vnc code was itself adapted from vncviewer for windows\n"
+"\n"
+" win2vnc, adapted from vncviewer by Fredrik Hubinette 2001\n"
+"\n"
+" Original copyright follows:\n"
+"\n"
+"  Copyright (C) 1999 AT&T Laboratories Cambridge. All Rights Reserved.\n"
+"\n"
+"  This file is part of the VNC system.\n"
+"\n"
+"  The VNC system is free software; you can redistribute it and/or modify\n"
+"  it under the terms of the GNU General Public License as published by\n"
+"  the Free Software Foundation; either version 2 of the License, or\n"
+"  (at your option) any later version.\n"
+"\n"
+"  This program is distributed in the hope that it will be useful,\n"
+"  but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"  GNU General Public License for more details.\n"
+"\n"
+"  You should have received a copy of the GNU General Public License\n"
+"  along with this program; if not, write to the Free Software\n"
+"  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,\n"
+"  USA.\n"
+"\n"
+" If the source code for the VNC system is not available from the place \n"
+" whence you received this file, check http://www.uk.research.att.com/vnc or contact\n"
+" the authors on vnc@uk.research.att.com for information on obtaining it.\n"
+#endif
+;
